d052ef6cda6f177df331b097c4ce11a24ab36158
amprajasri <amprajasri@gmail.com>
2024-10-08 22:38:51 +0530

    database port

--- server/server.js
+++ server/server.js
@@ -113,7 +113,7 @@ app.post('/api/book', (req, res) =>
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   
